aa2bdcf469e0c446420a78f1e9484c6085c74a35
hrming <hae913131@gmail.com>
2022-06-09 20:11:35 +0900

    first commit

---
+++ src/main/java/level1/q10171.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class q10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
+
+// ' " ＼와 같은 특수문자를 출력할 때는 해당 문자 앞에 백슬래시 (＼)를 넣어야 함
